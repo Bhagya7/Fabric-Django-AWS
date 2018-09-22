49d8df250110d0ed75aedeb561919ecf141fc6cd
Bhagyashri <bhagyashrikatambale7@gmail.com>
2018-09-22 15:59:36 +0800

    Second commit

--- fab_nginx.py
+++ fab_nginx.py
@@ -3,13 +3,15 @@ from fabric import Connection
 #from fabric.contrib import project
 
  
-
+nginx_conf = "/home/bhagyashri/virtualenv1/fab_config/default"
+sites_available = "/etc/nginx/sites-available"
+sites_enabled = "/etc/nginx/sites-enabled"
 # Main - install NGINX
 #print("Connecting to EC2 instance!")
 c = Connection('i-08fe404ec44f8c46e')
 
 #print("Installing NGINX!")
-result = c.run("sudo apt-get update; sudo apt install nginx; sudo cp ~/virtualenv1/default /etc/nginx/sites-enabled; sudo service nginx restart")
+result = c.run("sudo apt-get update; sudo apt install nginx; sudo cp %s %s; sudo service nginx restart" % (nginx_conf, sites_enabled))
 
 #print("Done!")
 #c.sudo("apt install nginx")
